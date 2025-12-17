1af8198bfe3091ef253792d518f3cda08ae415a4
bala-amypo <bala@amypo.in>
2025-12-17 09:39:31 +0000

    User commit

--- src/main/java/com/example/demo/ServiceImplementation/StudentServiceImplementation.java
+++ src/main/java/com/example/demo/ServiceImplementation/StudentServiceImplementation.java
@@ -2,10 +2,14 @@ package com.example.demo.ServiceImplementation;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
 
 @Service
 public class StudentServiceImplementation implements StudentService{
+    
+    private final StudentRepository StudentRepository
+
     public Student saveStudent(Student student){
         return 
     }
