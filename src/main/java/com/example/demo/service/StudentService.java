70235ac87627016f480ccd365989da04481fa606
bala-amypo <bala@amypo.in>
2025-12-18 04:45:38 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,17 +1,9 @@
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.entity.Student;
 
-public interface StudentService {
-
+public interface StudentService{
     Student saveStudent(Student student);
-
-    List<Student> getAllStudents();
-
-    Student getbyid(int id);
-
-    Student updatedata(int id, Student student);
-
-    Student deletedata(int id);
+    
 }
+
